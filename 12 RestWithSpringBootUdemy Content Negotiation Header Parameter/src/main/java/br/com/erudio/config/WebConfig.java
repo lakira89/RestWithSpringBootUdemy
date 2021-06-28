a0996fd1f711a9fd803c8a3d95c06499f668bab1
Akira <leonardo.matsuzaki@sefaz.am.gov.br>
2021-06-28 17:35:55 -0400

    Content Negotiation Header Parameter

---
+++ 12 RestWithSpringBootUdemy Content Negotiation Header Parameter/src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,41 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Via Extension. localhost:8080/person.json
+        /*  configurer.favorParameter(false)
+                // Não vai ignorar o que vier no cabeçalho da requisição
+                .ignoreAcceptHeader(false)
+                // Conteúdo padrão vai ser JSON
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                // Vai suportar as MediaTypes JSON e XML
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+        // Via QUERY PARAM. localhost:8080/person?mediaType=xml
+        /*configurer.favorPathExtension(false)
+                .favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+        configurer.favorPathExtension(false)
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
