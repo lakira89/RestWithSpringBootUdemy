672fb6808e26210d9bd593750dbad2992373b9b8
Akira <leonardo.matsuzaki@sefaz.am.gov.br>
2021-07-01 16:46:42 -0400

    CORS

---
+++ 16 RestWithSpringBootUdemy CORS/src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,56 @@
+package br.com.erudio.config;
+
+import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    // Vai definir CrossOrigin em toda aplicação
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE", "CONNECT");
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Via Extension. localhost:8080/person.json
+        /*  configurer.favorParameter(false)
+                // Não vai ignorar o que vier no cabeçalho da requisição
+                .ignoreAcceptHeader(false)
+                // Conteúdo padrão vai ser JSON
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                // Vai suportar as MediaTypes JSON e XML
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+        // Via QUERY PARAM. localhost:8080/person?mediaType=xml
+        /*configurer.favorPathExtension(false)
+                .favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+        configurer.favorPathExtension(false)
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_YML);
+    }
+}
