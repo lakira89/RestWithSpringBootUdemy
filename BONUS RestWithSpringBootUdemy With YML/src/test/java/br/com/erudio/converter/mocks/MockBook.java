9833b5ab18bd729faeae5d8011389f7e5e781e29
Akira <leonardo.matsuzaki@sefaz.am.gov.br>
2021-07-20 17:19:28 -0400

    Application Properties to YML

---
+++ BONUS RestWithSpringBootUdemy With YML/src/test/java/br/com/erudio/converter/mocks/MockBook.java
@@ -0,0 +1,59 @@
+package br.com.erudio.converter.mocks;
+
+import br.com.erudio.data.model.Book;
+import br.com.erudio.data.model.Person;
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.data.vo.v1.PersonVO;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MockBook {
+
+    public Book mockEntity() {
+        return mockEntity(0);
+    }
+
+    public BookVO mockVO() {
+        return mockVO(0);
+    }
+
+    public List<Book> mockEntityList() {
+        List<Book> books = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            books.add(mockEntity(i));
+        }
+        return books;
+    }
+
+    public List<BookVO> mockVOList() {
+        List<BookVO> books = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            books.add(mockVO(i));
+        }
+        return books;
+    }
+
+    private Book mockEntity(Integer number) {
+        Book book = new Book();
+        book.setId(number.longValue());
+        book.setAuthor("Author Test" + number);
+        book.setLaunchDate(LocalDate.of(2021, 1, 1 + number));
+        book.setPrice(BigDecimal.valueOf(number));
+        book.setTitle("Title Test" + number);
+        return book;
+    }
+
+    private BookVO mockVO(Integer number) {
+        BookVO book = new BookVO();
+        book.setKey(number.longValue());
+        book.setAuthor("Author Test" + number);
+        book.setLaunchDate(LocalDate.of(2021, 1, 1 + number));
+        book.setPrice(BigDecimal.valueOf(number));
+        book.setTitle("Title Test" + number);
+        return book;
+    }
+}
