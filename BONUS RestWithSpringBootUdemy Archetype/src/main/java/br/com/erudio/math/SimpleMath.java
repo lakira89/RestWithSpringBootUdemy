f9e87bea0474288fe7b97df3145dc3d7eb1cd27d
Akira <leonardo.matsuzaki@sefaz.am.gov.br>
2021-07-27 20:58:11 -0400

    Archetype

---
+++ BONUS RestWithSpringBootUdemy Archetype/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,27 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+    public Double sum(Double firstNumber, Double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public Double subtract(Double firstNumber, Double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public Double multiply(Double firstNumber, Double secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public Double divide(Double firstNumber, Double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public Double average(Double firstNumber, Double secondNumber) {
+        return (firstNumber + secondNumber) / 2;
+    }
+
+    public Double squareRoot(Double number) {
+        return Math.sqrt(number);
+    }
+}
